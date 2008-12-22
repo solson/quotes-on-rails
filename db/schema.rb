1acd00e863c4182bf16b429209c65972d023e76f
Scott Olson <scott@scott-olson.org>
2008-12-21 20:45:17 -0600

    added sessions migration so it actually works

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081015014126) do
+ActiveRecord::Schema.define(:version => 20081222023524) do
 
   create_table "admins", :force => true do |t|
     t.string   "login",                     :limit => 40
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(:version => 20081015014126) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
 
   create_table "votes", :force => true do |t|
     t.integer  "ip_id"
